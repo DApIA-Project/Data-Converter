c3baef0b406fae5bf9d8eb8ece67f4f24386dd9f
Helchida <morgan.leclercq89@gmail.com>
2023-08-25 13:34:57 +0200

    NDJSON to CSV OK

--- src/JsonToCsv.ts
+++ src/JsonToCsv.ts
@@ -1,31 +1,6 @@
 import { createObjectCsvStringifier } from 'csv-writer';
 import {buildBooleanValueForCsv, buildSquawkValueForCsv, buildTimestampValue, getDateToTimestamp} from "./utils/utils";
 
-/*function getHeaders(jsonData: any[]): string[] {
-    const headers = new Set<string>();
-    jsonData.forEach((item) => {
-        for (const key in item) {
-            headers.add(key);
-        }
-    });
-    return Array.from(headers);
-}
-export function convertJSONtoCSV(jsonContentString: string): string {
-    let jsonContent = JSON.parse(jsonContentString);
-    const headers = getHeaders(jsonContent);
-    const csvStringifier = createObjectCsvStringifier({
-        header: [
-            ...headers.map((header) => ({ id: header, title: header })),
-        ],
-    });
-
-
-
-    const csvString = csvStringifier.getHeaderString() + csvStringifier.stringifyRecords(jsonContent);
-    return csvString
-
-}*/
-
 function createCSVData(jsonData: any[]): any[] {
     let index = 1;
     const csvData: any[] = [];
